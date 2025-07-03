43f5f346c2ff1e1a61e12ff4e91ba0f45eccc4f3
touhid9pc <touhid@9point.capital>
2025-07-03 15:35:39 +0530

    fixed tooltip bug

--- src/components/cardsGrid/CardsGrid.tsx
+++ src/components/cardsGrid/CardsGrid.tsx
@@ -113,151 +113,152 @@ const CardsGrid: React.FC<CardsGridProps> = ({
   }, [user, selectedCard, setUser, setInterestsData, scrollToSection]);
 
   return (
-    <div className="min-h-screen px-2 sm:px-6 lg:px-8">
-      <div className="w-full flex flex-col items-center z-20 gap-4 mb-6 md:mb-10">
-        <motion.div
-          initial={{ y: 50, opacity: 0.1 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1 }}
-          className="flex flex-wrap justify-center gap-4"
-        >
-          {/* <button
+    <>
+      {tooltipIndex && (
+        <div className="fixed top-20 left-1/2 -translate-x-1/2  z-[10]  bg-gray-800 text-white text-xs px-3 py-1 w-max rounded shadow-lg pointer-events-none animate-fadeIn">
+          Long press to see products
+        </div>
+      )}
+      <div className="min-h-screen px-2 sm:px-6 lg:px-8">
+        <div className="w-full flex flex-col items-center z-20 gap-4 mb-6 md:mb-10">
+          {/* {tooltipIndex === idx && ( */}
+
+          <motion.div
+            initial={{ y: 50, opacity: 0.1 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="flex flex-wrap justify-center gap-4"
+          >
+            {/* <button
             className="px-4 py-1.5 text-xs sm:text-sm font-semibold rounded-full bg-gradient-to-r from-indigo-600 to-purple-600 text-white shadow-md hover:shadow-lg hover:scale-105 transition-all duration-200 flex items-center"
             onClick={handleReset}
           >
             <RotateCcw className="mr-1.5 h-4 w-4" /> Reset
           </button> */}
 
-          <AnimatedButton
-            className="text-base sm:text-lg flex justify-center items-center"
-            onClick={() => setSelectedCard([])}
-          >
-            {/* <Shuffle className="mr-1.5 h-4 w-4" /> */}
-            Clear
-          </AnimatedButton>
-          <AnimatedButton
-            className="text-base sm:text-lg flex justify-center items-center !bg-[#1a1a1a] !text-[#FAF9F6]"
-            onClick={handleShuffle}
-          >
-            {/* <Shuffle className="mr-1.5 h-4 w-4" /> */}
-            Shuffle
-          </AnimatedButton>
-        </motion.div>
-      </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 md:gap-8">
-        {shuffledData?.slice(0, 10).map((card, idx) => {
-          const isSelected = selectedCardIds.has(card?.id);
-          const isHovered = hoveredCard === idx;
-
-          const handleTouchStart = () => {
-            longPressTimeout.current = setTimeout(() => {
-              setHoveredCard(idx);
-              setTooltipIndex(null); // hide tooltip on long press
-            }, 500);
-
-            // Show tooltip briefly for short touches
-            setTooltipIndex(idx);
-            setTimeout(() => setTooltipIndex(null), 1200);
-          };
-
-          const handleTouchEnd = () => {
-            if (longPressTimeout.current)
-              clearTimeout(longPressTimeout.current);
-            setHoveredCard(null);
-          };
-
-          const handleTouchMove = () => {
-            if (longPressTimeout.current)
-              clearTimeout(longPressTimeout.current);
-            setHoveredCard(null);
-          };
-
-          console.log(tooltipIndex === idx);
-          return (
-            <motion.div
-              key={idx}
-              className="w-[9rem] h-[12rem] sm:w-[13rem] sm:h-[17rem] md:w-[15rem] md:h-[19rem] lg:h-[22rem] max-w-[17rem] max-h-[22rem] perspective-1000"
-              onMouseEnter={() => {
-                setTimeout(() => {
-                  setHoveredCard(idx);
-                }, 500);
-              }}
-              onMouseLeave={() => setHoveredCard(null)}
-              onTouchStart={handleTouchStart}
-              onTouchEnd={handleTouchEnd}
-              onTouchMove={handleTouchMove}
-              onClick={() => handleCardSelect(card)}
+            <AnimatedButton
+              className="text-base sm:text-lg flex justify-center items-center"
+              onClick={() => setSelectedCard([])}
             >
-              {tooltipIndex === idx && (
-                <div className=" bg-gray-800 text-white text-xs px-3 py-1 w-max rounded shadow-lg pointer-events-none animate-fadeIn">
-                  Long press to see products
-                </div>
-              )}
-              <motion.div
-                className="relative w-full h-full rounded-xl transition-transform duration-500"
-                animate={{ rotateY: isHovered ? 180 : 0 }}
-                transition={{ duration: 0.1 }}
-                style={{ transformStyle: "preserve-3d" }}
-              >
-                {/* FRONT */}
-                <div className="absolute inset-0 backface-hidden flex flex-col items-center justify-center bg-white rounded-xl p-6 shadow">
-                  {isSelected && (
-                    <div className="absolute top-2 right-2 z-30 rounded-full p-1 flex items-center justify-center bg-green-200">
-                      <Check
-                        size={30}
-                        color="#1a1a1a"
-                        className="drop-shadow-md"
-                      />
-                    </div>
-                  )}
+              {/* <Shuffle className="mr-1.5 h-4 w-4" /> */}
+              Clear
+            </AnimatedButton>
+            <AnimatedButton
+              className="text-base sm:text-lg flex justify-center items-center !bg-[#1a1a1a] !text-[#FAF9F6]"
+              onClick={handleShuffle}
+            >
+              {/* <Shuffle className="mr-1.5 h-4 w-4" /> */}
+              Shuffle
+            </AnimatedButton>
+          </motion.div>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 md:gap-8">
+          {shuffledData?.slice(0, 10).map((card, idx) => {
+            const isSelected = selectedCardIds.has(card?.id);
+            const isHovered = hoveredCard === idx;
 
-                  <div className="mb-6 h-[60%] flex items-center justify-center">
-                    <Image
-                      src={
-                        companiesImages[card.symbol as CompanySymbol] ||
-                        "/fallback.webp"
-                      }
-                      alt={card.name}
-                      width={100}
-                      height={100}
-                      className="transition-transform duration-300 hover:scale-110 "
-                    />
-                    {/* w-12 h-12 sm:w-14 sm:h-14 md:w-max md:h-max */}
+            const handleTouchStart = () => {
+              longPressTimeout.current = setTimeout(() => {
+                setHoveredCard(idx);
+                setTooltipIndex(null); // hide tooltip on long press
+              }, 500);
+
+              // Show tooltip briefly for short touches
+              setTooltipIndex(idx);
+              setTimeout(() => setTooltipIndex(null), 1200);
+            };
+
+            const handleTouchEnd = () => {
+              if (longPressTimeout.current)
+                clearTimeout(longPressTimeout.current);
+              setHoveredCard(null);
+            };
+
+            const handleTouchMove = () => {
+              if (longPressTimeout.current)
+                clearTimeout(longPressTimeout.current);
+              setHoveredCard(null);
+            };
+            return (
+              <motion.div
+                key={idx}
+                className="w-[9rem] h-[12rem] sm:w-[13rem] sm:h-[17rem] md:w-[15rem] md:h-[19rem] lg:h-[22rem] max-w-[17rem] max-h-[22rem] perspective-1000"
+                onMouseEnter={() => {
+                  setTimeout(() => {
+                    setHoveredCard(idx);
+                  }, 500);
+                }}
+                onMouseLeave={() => setHoveredCard(null)}
+                onTouchStart={handleTouchStart}
+                onTouchEnd={handleTouchEnd}
+                onTouchMove={handleTouchMove}
+                onClick={() => handleCardSelect(card)}
+              >
+                <motion.div
+                  className="relative w-full h-full rounded-xl transition-transform duration-500"
+                  animate={{ rotateY: isHovered ? 180 : 0 }}
+                  transition={{ duration: 0.1 }}
+                  style={{ transformStyle: "preserve-3d" }}
+                >
+                  {/* FRONT */}
+                  <div className="absolute inset-0 backface-hidden flex flex-col items-center justify-center bg-white rounded-xl p-6 shadow">
+                    {isSelected && (
+                      <div className="absolute top-2 right-2 z-30 rounded-full p-1 flex items-center justify-center bg-green-200">
+                        <Check
+                          size={30}
+                          color="#1a1a1a"
+                          className="drop-shadow-md"
+                        />
+                      </div>
+                    )}
+
+                    <div className="mb-6 h-[60%] flex items-center justify-center">
+                      <Image
+                        src={
+                          companiesImages[card.symbol as CompanySymbol] ||
+                          "/fallback.webp"
+                        }
+                        alt={card.name}
+                        width={100}
+                        height={100}
+                        className="transition-transform duration-300 hover:scale-110 "
+                      />
+                      {/* w-12 h-12 sm:w-14 sm:h-14 md:w-max md:h-max */}
+                    </div>
+
+                    <h3
+                      className={`text-xs sm:text-sm md:text-xl font-bold capitalize text-center ${
+                        isSelected ? "text-green-700" : "text-gray-800"
+                      }`}
+                    >
+                      {card.name}
+                    </h3>
                   </div>
 
-                  <h3
-                    className={`text-xs sm:text-sm md:text-xl font-bold capitalize text-center ${
-                      isSelected ? "text-green-700" : "text-gray-800"
-                    }`}
+                  {/* BACK */}
+                  <div
+                    className={`absolute inset-0 backface-hidden rotate-y-180 flex flex-col items-start justify-center  ${
+                      isSelected
+                        ? "bg-green-100/20 border border-green-200"
+                        : "bg-white"
+                    }  rounded-xl p-5 shadow`}
                   >
-                    {card.name}
-                  </h3>
-                </div>
-
-                {/* BACK */}
-                <div
-                  className={`absolute inset-0 backface-hidden rotate-y-180 flex flex-col items-start justify-center  ${
-                    isSelected
-                      ? "bg-green-100/20 border border-green-200"
-                      : "bg-white"
-                  }  rounded-xl p-5 shadow`}
-                >
-                  <h4 className="text-xs sm:text-sm md:text-lg font-semibold text-gray-700 mb-2 w-full text-center">
-                    Top Products
-                  </h4>
-                  <ul className="list-disc list-inside text-gray-600 text-xs  sm:text-sm font-semibold space-y-1 overflow-y-auto no-scrollbar max-h-[90%]">
-                    {(card.topProducts ?? []).map((product, i) => (
-                      <li key={i}>{product}</li>
-                    ))}
-                  </ul>
-                </div>
+                    <h4 className="text-xs sm:text-sm md:text-lg font-semibold text-gray-700 mb-2 w-full text-center">
+                      Top Products
+                    </h4>
+                    <ul className="list-disc list-inside text-gray-600 text-xs  sm:text-sm font-semibold space-y-1 overflow-y-auto no-scrollbar max-h-[90%]">
+                      {(card.topProducts ?? []).map((product, i) => (
+                        <li key={i}>{product}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </motion.div>
               </motion.div>
-            </motion.div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
 
-      {/* <div className="text-center mt-20">
+        {/* <div className="text-center mt-20">
         <div className="mb-6">
           <span className="text-lg font-semibold text-gray-700">
             Selected: {selectedCard.length}/4 companies
@@ -271,20 +272,21 @@ const CardsGrid: React.FC<CardsGridProps> = ({
         </button>
       </div> */}
 
-      <div className="w-full flex justify-center mt-6 md:mt-8 z-30 ">
-        {selectedCard?.length >= 4 ? (
-          <AnimatedButton
-            name="Reveal"
-            className="text-base sm:text-lg font-semibold rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-all duration-200 flex items-center"
-            onClick={handleReveal}
-          />
-        ) : (
-          <p className="text-center text-lg md:text-xl font-semibold text-gray-800">
-            {`Selected ${selectedCard?.length}/4`}
-          </p>
-        )}
+        <div className="w-full flex justify-center mt-6 md:mt-8 z-30 ">
+          {selectedCard?.length >= 4 ? (
+            <AnimatedButton
+              name="Reveal"
+              className="text-base sm:text-lg font-semibold rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-all duration-200 flex items-center"
+              onClick={handleReveal}
+            />
+          ) : (
+            <p className="text-center text-lg md:text-xl font-semibold text-gray-800">
+              {`Selected ${selectedCard?.length}/4`}
+            </p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
