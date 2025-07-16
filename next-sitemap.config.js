a0c736e223726c807b4d67cbed3698994012bd1a
kesavamurthyk <kesavan.k2014@gmail.com>
2025-07-16 19:12:28 +0530

    fix: changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://graviti.finance',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'daily',
+    priority: 0.7,
+};
